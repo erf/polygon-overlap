093fc4b616babcc5dfede04bd52560f496378ba6
Erlend Fagerheim <erlendf80@gmail.com>
2015-12-19 03:29:06 +0100

    vip

--- index.js
+++ index.js
@@ -4,16 +4,15 @@ var intersect = require("lines-intersect");
 var inside = require("point-inside-polygon");
 
 function polygon_points_inside(p0, p1) {
-    for (var i = 0; i < p0.length; i += 1) {
-        for (var j = 0; j < p1.length; j += 1) {
-
-            if (inside(p0[i], p1)) {
-                return true;
-            }
-
-            if (inside(p1[j], p0)) {
-                return true;
-            }
+    var i;
+    for (i = 0; i < p0.length; i += 1) {
+        if (inside(p0[i], p1)) {
+            return true;
+        }
+    }
+    for (i = 0; i < p1.length; i += 1) {
+        if (inside(p1[i], p0)) {
+            return true;
         }
     }
     return false;
@@ -22,7 +21,8 @@ function polygon_points_inside(p0, p1) {
 function polygon_edges_overlap(p0, p1) {
     for (var i = 0; i < p0.length - 1; i += 1) {
         for (var j = 0; j < p1.length - 1; j += 1) {
-            if (intersect(p0[i][0], p0[i][1], p0[i + 1][0], p0[i + 1][1], p1[j][0], p1[j][1], p1[j + 1][0], p1[j + 1][1])) {
+            if (intersect(p0[i][0], p0[i][1], p0[i + 1][0], p0[i + 1][1], 
+                          p1[j][0], p1[j][1], p1[j + 1][0], p1[j + 1][1])) {
                 return true;
             }
         }
